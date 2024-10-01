70c2b4db83e399705965b6a340af42f902df2dc3
TomStranger <max.xavier.white@gmail.com>
2024-10-01 09:26:48 -0400

    OCT commit 1

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-with open("books/frankenstein.txt") as f:
-    # ...
+def main():
+    
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        return file_contents
+
+    def count_words(file_contents):
+        words = file_contents.split()
+        return print(len(words))
+
+    def count_char(words):
+        lowered_words = words.lower()
+        
+    
+    
+    
+    
+    count_words(file_contents)
+
+main()
