a4c931f3ae4735246a48233589052b17d260838b
TomStranger <max.xavier.white@gmail.com>
2024-10-01 10:01:41 -0400

    Counting words and characters are working as I'd like.

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 def main():
-    
+    char_dict = {}
+
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        return file_contents
 
     def count_words(file_contents):
         words = file_contents.split()
-        return print(len(words))
+        return print(f"There are {len(words)} words in this document.")
 
-    def count_char(words):
-        lowered_words = words.lower()
+
+    def count_char(file_contents):
+        lowered_file = file_contents.lower()
+
+        for char in lowered_file:
+            if char in char_dict:
+                char_dict[char] += 1
+            else:
+                char_dict[char] = 1
         
-    
-    
-    
-    
+        return print(char_dict)
+
     count_words(file_contents)
+    count_char(file_contents)
 
 main()
