992d17b182910f8e534604f4ccdadf4e8ae588eb
TomStranger <max.xavier.white@gmail.com>
2024-10-01 10:58:39 -0400

    halting work for now

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 def main():
+    
+    #Preperatory dictionary declaration and output/report message.
     char_dict = {}
-
+    char_list = []
+    print("--- Begin Book Report of books/frankenstein.txt ---")
+    
+    #Open and read the text, define important variable for functions
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
 
     def count_words(file_contents):
         words = file_contents.split()
         return print(f"There are {len(words)} words in this document.")
-
 
     def count_char(file_contents):
         lowered_file = file_contents.lower()
@@ -18,9 +22,23 @@ def main():
             else:
                 char_dict[char] = 1
         
-        return print(char_dict)
+        return char_dict
+
+
+    ###resume work here:
+
+    def sort_on(char_dict):
+        return char_dict("char")
+    
+    def make_list(char_dict):
+        
+
+        return char_list
+    
+
 
     count_words(file_contents)
     count_char(file_contents)
+    print(char_list)
 
 main()
